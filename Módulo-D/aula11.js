a4afb9296e9f233ca61203d36af9912cedc8e6eb
vitoriarbs <160158820+vitoriarbs@users.noreply.github.com>
2024-05-07 22:10:10 -0300

    módulo c e d

---
+++ Módulo-D/aula11.js
@@ -0,0 +1,7 @@
+var vel = 33
+
+console.log(`a velocidade do seu carro é ${vel} km/h`)
+if (vel > 67) { //condição simples
+    console.log('você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log(`Dirija sempre usando cinto de segurança`)
