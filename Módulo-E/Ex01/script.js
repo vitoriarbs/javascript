7ba43a23bbbed8dd201d2a01958317ac29e93b0e
vitoriarbs <160158820+vitoriarbs@users.noreply.github.com>
2024-05-10 21:05:00 -0300

    js módulo e ex

--- Módulo-E/Ex01/script.js
+++ Módulo-E/Ex01/script.js
@@ -6,6 +6,17 @@ function contador(){
     let c  = Number(window.document.getElementById('pas').value)
     var res = window.document.getElementById('res')
     
+    if (n1.value.length == 0 || n2.value.length == 0 || c.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!')
+    } /* else {//como o professor resolveu
+        res.innerHTML = 'Contando: '
+        let i = Number(n1,value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        for(let c = i; c <= f; c += p){
+            res.innerHTML += `${c} \u{1F603}`
+        }*/
 
     while(n1 <= n2) {
         res.innerHTML += `${n1}.. `
